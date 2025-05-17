68cc58dee8a947558437a630e8e8b8ade365af4d
eyanreni22 <devswathi.24@gmail.com>
2025-05-17 16:09:48 +0530

    index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173", // Frontend development URL
-      "https://frontend.vercel.app", // Frontend production URL
+      "https://our-services-mern-frontend.vercel.app", // Frontend production URL
     ],
     credentials: true,
   },
